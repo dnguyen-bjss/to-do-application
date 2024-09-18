f0c5e544527f91a5163bf4076e91c625aa089a07
Dat Nguyen <dat.nguyen@bjss.com>
2024-09-18 13:06:16 +0100

    Part 1-XX: Created folders to track iterations of the application

---
+++ part1-11/to_do_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestToDo(t *testing.T) {
+	todo := ToDo{
+		ID:          1,
+		Description: "Complete assignment",
+		Completed:   false,
+	}
+	
+	got := todo.String()
+	want := "ID: 1, Description: Complete assignment, Completed: false"
+	
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestDisplay(t *testing.T) {
+	todo1 := ToDo{
+		ID:          1,
+		Description: "Wash dishes",
+		Completed:   false,
+	}
+	todo2 := ToDo{
+		ID:          2,
+		Description: "Hoover the house",
+		Completed:   true,
+	}
+	buffer := bytes.Buffer{}
+	Display(&buffer, todo1, todo2)
+
+	got := buffer.String()
+	want := "{\"id\":1,\"description\":\"Wash dishes\",\"completed\":false}\n{\"id\":2,\"description\":\"Hoover the house\",\"completed\":true}\n"
+	
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
